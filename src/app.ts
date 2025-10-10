be8c19803f27ff07b66693a778e5dfc340c3da6e
ronaldoapf <ronaldo.alves.1997@gmail.com>
2025-10-10 19:51:09 -0300

    feat: add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,29 @@
 import fastify from "fastify";
 import { env } from "./config/env";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import { jsonSchemaTransform } from "fastify-type-provider-zod";
 
 export const app = fastify()
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 app.listen({
   port: env.PORT,
 }).then(() => {
   console.log(`🚀 Server is running at ${env.API_URL}:${env.PORT}`)
+  console.log(`🚀 Docs is running at ${env.API_URL}:${env.PORT}/docs`)
+
 })
